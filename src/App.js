8663a470ed66d3edf8207ee9577f075937ac8ece
Aboubakary833 <aboubakarycisse472@gmail.com>
2021-01-28 16:22:37 +0100

    Commit de mon Todo List avec ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  input = React.createRef();
+  state = {
+    todos: [
+      {id: 0, todo: 'Cuisine'},
+      {id: 1, todo: 'Vaisellle'},
+      {id: 2, todo: 'Courses'}
+    ]
+  };
+  delTodo = id => {
+    let myCopy = this.state.todos.slice();
+    const index= myCopy.findIndex(todo => todo.id === id);
+    myCopy.splice(index, 1);
+    this.setState({todos: myCopy});
+    console.log(this.state.todos);
+  }
+  addTodo = e => {
+    e.preventDefault();
+    const myCopy = this.state.todos.slice();
+    const id = myCopy.length;
+    const todo = this.input.current.value;
+    const newTodo = {id, todo};
+    myCopy.push(newTodo);
+    this.setState({todos: myCopy});
+    this.input.current.value = ''
+  }
 
-export default App;
+  render() {
+    return (
+      <div id="container">
+        <h1>My ToDo List</h1>
+        <ul>
+          {
+          this.state.todos.map(el => {
+            return <li>
+                      <span>{el.todo}</span>
+                      <button onClick = {() => {this.delTodo(el.id)} }>
+                      <div className="lign"></div>
+                      <div className="lign"></div>
+                      </button>
+                    </li>
+          })}
+        </ul>
+        <form onSubmit={this.addTodo}>
+          <input type="text" ref={this.input} placeholder="Nom d'une tâche à faire" />
+          <button type="submit">Ajouter</button>
+        </form>
+      </div>
+    );
+  }
+}
